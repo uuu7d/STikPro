59c1f0fb4e4fa575a72dd14aff947beacf6750de
Raed / Dev-land <rrxs511@gmail.com>
2026-09-23 19:24:24 +0300

    Create TikTok.h

---
+++ TikTok.h
@@ -0,0 +1,13 @@
+#import <UIKit/UIKit.h>
+
+@interface UIView (StoryViewController)
+- (UIViewController *)viewController;
+@end
+
+@interface TTKStoryDetailTableViewCell : UITableViewCell
+- (UIViewController *)viewController;
+@end
+
+@interface AWEAwemeStatusModel : NSObject
+- (void)setVideoMuteModel:(id)arg1;
+@end
