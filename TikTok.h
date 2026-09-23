7fd38debc31033e1ee8cb91d5265562def3f2bc3
Raed / Dev-land <rrxs511@gmail.com>
2026-09-23 23:08:56 +0300

    Refactor TikTok.h by removing old methods
    Removed deprecated method declarations and added new class interface for TTKRichContentPlayerViewController.

--- TikTok.h
+++ TikTok.h
@@ -1,13 +1,7 @@
 #import <UIKit/UIKit.h>
 
-@interface UIView (StoryViewController)
-- (UIViewController *)viewController;
-@end
-
-@interface TTKStoryDetailTableViewCell : UITableViewCell
-- (UIViewController *)viewController;
-
-// تصريح عن الدوال الجديدة لتفادي خطأ المترجم (no visible @interface)
+// التصريح عن الكلاسات التي أظهرتها أداة FLEX
+@interface TTKRichContentPlayerViewController : UIViewController
 - (void)setupStoryDownloadButton;
 - (void)handleStoryDownloadTap:(UIButton *)sender;
 - (void)downloadFileFromURL:(NSURL *)url saveToPath:(NSString *)path;
